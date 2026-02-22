c79f489b98730e350ed06b493d4cd0751598d053
Gean Carlos <geancarlsilva@gmail.com>
2026-02-22 15:30:47 -0300

    feat: Criando class Usuario

---
+++ src/Usuario.java
@@ -0,0 +1,22 @@
+package src;
+
+public class Usuario {
+    String nome;
+    double peso;
+    double altura;
+
+    //metodo para calcular o peso do usuario
+
+    public double calcularIMC(){
+        return peso / (altura * altura);
+    }
+
+    /// Metodo para exibição no console
+    public void exibirFicha(){
+        System.out.println("--Ficha do Atleta--");
+        System.out.println("Nome :" + nome);
+        System.out.println("IMC:" + String.format("%.2f", calcularIMC()));
+
+        System.out.println("___________");
+    }
+}
